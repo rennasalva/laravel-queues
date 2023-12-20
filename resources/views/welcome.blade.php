c6af533fb5a45e2b01f1b7444b2de8fce0b165fa
rennasalva <rsalvatori@perfroce.com>
2023-12-20 15:18:44 +0100

    add new permission

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link href="http://laravel.css.localhost/css/app.css" rel="stylesheet" type="text/css" >
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
